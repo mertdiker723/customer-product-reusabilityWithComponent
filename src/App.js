dcce2b47a00e4a60915d67c89feb0b022b9d6c0c
Mert Diker <mertdiker723@gmail.com>
2021-09-13 14:40:49 +0300

    baştan

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
-import Movies from './components/movies.jsx'
+
 class App extends Component {
   render() {
     return (
-      <div className="container mt-2">
-        <Movies />
+      <div>
+
       </div>
     )
   }
 }
-
 
 export default App;
